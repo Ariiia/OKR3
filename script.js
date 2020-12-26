77bdab9c8976db8381d1b5f54ff5a65bd3f665c0
Dariia Kariavka <koalaansthen@gmail.com>
2020-12-26 05:19:01 +0200

    cursor

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 window.addEventListener("scroll", function(){
-let header=document.querySelector("header");
+
+// menu
+    let header=document.querySelector("header");
 header.classList.toggle("sticky", window.scrollY>0);
+
+})
+// cursor
+const cursor=document.querySelector(".cursor");
+document.addEventListener("mousemove",(e)=>{
+    cursor.style.left=e.clientX+'px';
+    cursor.style.top=e.clientY+'px';
 
 })
